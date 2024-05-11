8c2ab2803dcefb3f52376f4cf24c035756065224
harmooony <sava061205@gmail.com>
2024-05-11 11:46:53 +0300

    Начало положено

---
+++ main_scripts.js
@@ -0,0 +1,24 @@
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function dropdown_team1() {
+    document.getElementById("myDropdown_t1").classList.toggle("show");
+  }
+
+  function dropdown_team2() {
+    document.getElementById("myDropdown_t2").classList.toggle("show");
+  }
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
